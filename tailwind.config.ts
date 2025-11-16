9f403c24c01488a2f5abfd1585897faa9f9c25e7
Liow17 <jinhong2004@yahoo.com>
2025-11-16 16:58:04 +0800

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,16 @@
+cat > tailwind.config.ts << 'EOF'
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+export default config;
+EOF
